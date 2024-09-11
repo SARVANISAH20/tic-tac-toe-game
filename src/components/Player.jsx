eb16e29e4f0acec49e2432014b8b6dad0c70e34e
Sarvani Sah <sarvani@Sarvanis-MacBook-Air.local>
2024-09-12 00:17:01 +0530

    Tic Tac Toe

---
+++ src/components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
+
+    function handleEditClick() {
+        setIsEditing((editing) => !editing); //if your new state depends on your previous state value, you should pass a function to your state updating function
+        if (isEditing) {
+            onChangeName(symbol, playerName);
+        }
+    }
+
+    function handleChange(event) {
+        console.log(event);
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+    if (isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
+    }
+    return (
+        <li className={isActive ? 'active' : undefined}>
+            <span className="player">
+                {editablePlayerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+        </li>
+    );
+}
